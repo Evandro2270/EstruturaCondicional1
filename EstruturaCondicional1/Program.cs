72ea50d7b707f365a270c0420de2aca851d3fa06
Evandro2270 <evandro535@gmail.com>
2024-12-05 10:35:53 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaCondicional1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+                
+                Console.WriteLine("Qual hora atual? ");
+                int hora = int.Parse(Console.ReadLine());
+
+                if (hora < 12)
+                {
+                    Console.WriteLine("Bom dia! ");
+                }
+                else if (hora < 18)
+                {
+                    Console.WriteLine("Boa tarde! ");
+                }
+                else {
+                    Console.WriteLine("Boa noite! ");
+                }
+
+
+            }
+        }
+    }
+}
